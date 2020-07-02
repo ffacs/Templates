7656ab353c79451686e030850766a4858d83c31f
root <root@DESKTOP-PBGJ118.localdomain>
2020-07-02 15:11:28 +0800

    2020/7/2

--- cpp.cpp
+++ cpp.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
 const int maxn=1e6+5;
 vector<ll> V;
-int pre[maxn],a[maxn],b[maxn];
+int pre[maxn];
 int main() {
     
     return 0;
