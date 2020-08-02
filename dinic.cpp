da919a2b516924c7d56d820a0c48caf532c156f7
root <root@FFACS.localdomain>
2020-08-02 22:22:57 +0800

    dinic

--- dinic.cpp
+++ dinic.cpp
@@ -4,37 +4,37 @@ typedef long long ll;
 const int maxn=1e4+4;
 const int maxm=1e5+5;
 struct EDGE{
-	int t,nex;
+	int v,nex;
 	ll w;
 }edge[maxm<<1];
 int head[maxn],ecnt;
 inline void addedge(int u,int v,int w){
 	edge[++ecnt]={v,head[u],w};head[u]=ecnt;
 }
-int S,T,N,M;
+int s,t,N,m;
 const int INF=0x7fffffff;
 int degree[maxn];
 inline bool bfs(){
 	queue<int> Q;
-	Q.push(S);
-	for(int i=1;i<=N;i++) degree[i]=0;degree[S]=1;
+	Q.push(s);
+	for(int i=1;i<=N;i++) degree[i]=0;degree[s]=1;
 	while (!Q.empty()){
 		int x=Q.front();Q.pop();
 		for(int i=head[x];i;i=edge[i].nex){
-			int v=edge[i].t;
+			int v=edge[i].v;
 			if(edge[i].w&&!degree[v]){
 				degree[v]=degree[x]+1;
 				Q.push(v);
 			}
 		}
 	}
-	return degree[T];
+	return degree[t];
 }
 ll dfs(int now,ll flow){
-	if(now==T||!flow) return flow;
+	if(now==t||!flow) return flow;
 	ll ans=0;
 	for(int i=head[now];i;i=edge[i].nex){
-		int v=edge[i].t,low;
+		int v=edge[i].v,low;
 		if(degree[now]+1==degree[v]&&edge[i].w){
 			low=dfs(v,min(flow,edge[i].w));
 			edge[i].w-=low;edge[((i-1)^1)+1].w+=low;
@@ -46,11 +46,11 @@ ll dfs(int now,ll flow){
 }
 inline ll dinic(){
 	ll ans=0,d;
-	while (bfs()) while (d=dfs(S,INF)) ans+=d;
+	while (bfs()) while (d=dfs(s,INF)) ans+=d;
 	return ans;
 }
 int main(){
-	cin>>N>>M>>S>>T; //N V,M E,S 源,T 汇,需初始 N,S,T 
+	cin>>N>>m>>s>>t; //N: V,m: E,s: 源,t: 汇,需初始 N,s,t 
 	int u,v,w;
 	for(int i=1;i<=M;i++){
 		scanf("%d%d%d",&u,&v,&w);
